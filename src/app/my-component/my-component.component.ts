8ae0fc8f0e563cdcbd063c674f4eab7a8069a4ef
akshayblevel <akshayblevel@gmail.com>
2021-08-20 18:20:46 +0530

    3 files modified, 3 added, 1 deleted

---
+++ src/app/my-component/my-component.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-component',
+  templateUrl: './my-component.component.html',
+  styleUrls: ['./my-component.component.css']
+})
+export class MyComponentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  Title = 'Component Linked Template';
+}
